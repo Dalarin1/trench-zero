48e18a76fd885a9c8233e3e7a9bb8069577b2d97
Dalarin1 <iluchin_vlados@mail.ru>
2026-03-14 13:56:28 +0500

    [feat] Exit script

---
+++ Assets/ClickController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ClickController : MonoBehaviour
+{
+    public Camera cam;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("ExitDoor"))
+                {
+                    Application.Quit();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/DoorExit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DoorExit : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        QuitGame();
+    }
+
+    void QuitGame()
+    {
+        Debug.Log("Game Closed");
+
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
